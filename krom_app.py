133bf729e21627e57f4bf5f4d78a1d3731810653
Michael Tran <michaeltran262@gmail.com>
2023-06-16 17:43:51 +0200

    Add pytest

--- krom_app.py
+++ krom_app.py
@@ -9,6 +9,8 @@ celery_app = flask_app.extensions["celery"]
 
 @flask_app.cli.command()
 def test():
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    #import unittest
+    #tests = unittest.TestLoader().discover('tests')
+    #unittest.TextTestRunner(verbosity=2).run(tests)
+    import pytest
+    pytest.main(['tests', '-v'])
--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,7 +1,7 @@
-import unittest
+import pytest
 from flask import current_app
 from krom_app import create_app
-
+'''
 class BasicsTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app('development')
@@ -15,4 +15,14 @@ class BasicsTestCase(unittest.TestCase):
         self.assertFalse(current_app is None)
 
     def test_app_is_testing(self):
-        self.assertTrue(current_app.config['MZK_IP'])
+        self.assertTrue(current_app.config['MZK_IP'])'''
+
+class TestApp:
+
+    def test_app_exists(self):
+        assert current_app
+
+    def test_app_has_valid_ip(self):
+        valid_ips = ['10.2.0.8', '10.223.1.8']
+        assert current_app.config['MZK_IP'] in valid_ips 
+        
